96bf03605b2a2a9456fa571ba79f04e8915e4450
Gabriel <g4brieldcs@gmail.com>
2024-09-12 22:09:18 -0300

    interface de perfil, porém incompleta ainda

--- src/script/perfil.js
+++ src/script/perfil.js
@@ -10,3 +10,22 @@ btnHome.addEventListener('click', e => {
 
     window.location.href = `/src/pages/home.html`
 })
+
+
+const btnTrocarPerfil = document.querySelector('#trocarPerfil')
+btnTrocarPerfil.addEventListener('click' , e => {
+    const uploadImage = document.querySelector('#uploadImage')
+    const display = document.querySelector('#sectionFeed')
+
+    uploadImage.style.display = 'flex'
+    display.style.backgroundColor = 'rgb(0, 0, 0, 0.2)'
+})
+
+const btnFecharUpload = document.querySelector('#btnFechar')
+btnFecharUpload.addEventListener('click' , e => {
+    const uploadImage = document.querySelector('#uploadImage')
+    const display = document.querySelector('#sectionFeed')
+
+    uploadImage.style.display = 'none'
+    display.style.backgroundColor = 'white'
+})
